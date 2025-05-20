00211931063010473ab13c0b923accb9d0370c59
Quality Gatekeeper <qgintest@gmail.com>
2025-05-19 21:10:20 -0400

    multiline cell example

--- ToDoList/apps/textToPdf/generate.py
+++ ToDoList/apps/textToPdf/generate.py
@@ -1,3 +1,4 @@
+from click import style
 from fpdf import FPDF
 import glob
 from pathlib import Path
@@ -11,7 +12,14 @@ for filepath in filepaths:
     pdf.add_page()
     name = Path(filepath).stem
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"{name.title()}")
+    pdf.cell(w=50, h=8, txt=f"{name.title()}", ln=1)
+
+    with open(filepath, 'r') as file:
+        content = file.read()
+
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=5, txt=content)
+    #print(content)
 
 
 pdf.output("PDFFile.pdf")
